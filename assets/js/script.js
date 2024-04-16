8a5c487ec916aa97abfb5795977e02fb383b0e81
rinklepoonia <rinklepoonia889@gmail.com>
2024-04-16 22:13:32 +0530

    manage padding of all section , apply back to tp

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,16 @@ menuIcon.addEventListener("click", function () {
     document.body.classList.toggle("overFlowHidden")
     document.querySelector(".menuIcon").classList.toggle("cross")
 })
+const topBtn = document.querySelector(".topBtn");
+const show = document.querySelector(".show");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 500) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+});
